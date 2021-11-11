8a87d1c21abfade125e37393ec64c0b61208f21e
Hernandes Erick <erickhernandes4@gmail.com>
2021-11-11 10:39:44 -0300

    implementado classe de usuario

---
+++ User.py
@@ -0,0 +1,9 @@
+class  User:
+    def __init__(self,firstName,lastName,RG,Id, adress,password):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.RG = RG
+        self.Id = Id
+        self.adress = adress
+        self.password = password
+
